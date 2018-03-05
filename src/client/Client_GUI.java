0ae32dcbab56ad9704da05370b6c82b0f13900c9
adriangunea <35837269+adriangunea@users.noreply.github.com>
2018-03-05 12:50:26 +0100

    asd
    asd

--- src/client/Client_GUI.java
+++ src/client/Client_GUI.java
@@ -2,11 +2,53 @@
     In questa classe viene gestito il Client
  */
 package client;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.awt.*;
 
-/**
- *
- * @author Pryze
- */
-public class Client_GUI {
+
+public class Client_GUI extends Frame{
+
+    Button gioca = new Button("gioca");
+    TextField nome = new TextField("inserisce il tuo nome");
+    Gest EU = new Gest();
+    Label benvenuto = new Label("benvenuto");
+    
+    public Client_GUI(){
+        this.setLayout(null);
+        
+        this.add(gioca);
+        gioca.setBounds(220,20,90,30);
+        gioca.addActionListener(EU);
+        
+        this.add(nome);
+        nome.setBounds(20,20,200,80);
+        
+        this.add(benvenuto);
+        benvenuto.setBounds(10,10,100,10);
+
+        
+        this.setTitle("TypeRace");
+        this.setLocation(200, 200);
+        this.setSize(400, 200);
+        this.setVisible(true);
+        
+    }
+    
+class Gest implements ActionListener{
+    
+    public void actionPerformed(ActionEvent E) {
+    
+    }
+}
+    
+    
+    public static void main(String[] args) {
+        new Client_GUI();
+    }
     
 }
+
+    
+
