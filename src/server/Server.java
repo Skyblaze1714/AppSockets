71115f28d75f6f493f51db745c0f760c689324cd
Frost Gear <danielelasagno@gmail.com>
2018-03-12 13:39:02 +0100

    Updated server

--- src/server/Server.java
+++ src/server/Server.java
@@ -12,27 +12,71 @@ import commons.*;
 public class Server {
     
     //Attributi
-    //Manager
-    private ServerSocketManager socket;
+    private static Quiz[] quizzes;
+    private static ServerSocketManager socket;
     
-    //Dati
-    private Quiz[] quizzes;
-    
-    
-    //Costruttore
-    public Server() {
+    //Metodi
+    private static void init(){
         quizzes = ServerStorageManager.loadArrayQuiz();
         socket = new ServerSocketManager();
     }
     
+    private static Message elaborateRequest(Message request){
+        Message response;
+            
+        switch (request.method) {
+            case "getQuiz":
+                System.out.println("server socket: sending quizzes to the client");
+                response = new Message(quizzes.clone());
+                break;
+                
+            case "postQuiz":
+                System.out.println("server socket: recivied a quiz = " + request.content.toString());
+                quizzes.clone();
+                response = new Message("outcome", true);
+                break;
+                
+            default:
+                System.out.println("server socket: failed to elaborate request");
+                response = new Message("outcome", false);
+                break;
+        }
+        
+        return response;
+    }
     
-    //Metodi
+    private void addQuiz(Quiz quiz){
+        //Inizializzazione del nuovo array
+        Quiz[] newQuizzes = new Quiz[quizzes.length + 1];
+        
+        //Copia del vecchio arry in quello nuovo
+        for(int i = 0; i quizzes.length; i++){
+            
+        }
+    }
     
     
     //Main
     public static void main(String[] args) {
         //Inizializzazione del server
-        Server server = new Server();
+        init();
+        
+        //TEST
+        Quiz[] quizzes1 = {
+            new Quiz("domanda", "autore", null, 1),
+            new Quiz("domanda","autore", null, 1),
+            new Quiz("domanda", "autore", null, 1)
+        };
+        quizzes = quizzes1;
+        
+        //Ciclo per la ricezione delle richiesquizzes1te al server
+        while(true) {
+            Message request = socket.waitForRequest();
+            
+            Message response = elaborateRequest(request);
+            
+            socket.sendResponse(response);
+        }
     }
     
 }
